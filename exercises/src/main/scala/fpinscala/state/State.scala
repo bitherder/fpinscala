9071a02b3b1f6acb6f91261f622fa2f92e2ecbb4
Larry Baltz <larry@baltz.org>
2017-04-30 15:29:49 +0200

    through 6.9

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -23,11 +23,12 @@ object RNG {
 
   def unit[A](a: A): Rand[A] = rng => (a, rng)
 
-  def map[A,B](s: Rand[A])(f: A => B): Rand[B] =
-    rng => {
-      val (a, rng2) = s(rng)
-      (f(a), rng2)
-    }
+  // def map[A,B](s: Rand[A])(f: A => B): Rand[B] =
+  //   rng => {
+  //     val (a, rng2) = s(rng)
+  //     (f(a), rng2)
+  //   }
+  def map[A,B](s: Rand[A])(f: A => B): Rand[B] = flatMap(s)(i => unit(f(i)))
 
   def nonNegativeInt(rng: RNG): (Int, RNG) = {
     val (rawVal, newRng) = rng.nextInt
@@ -83,13 +84,16 @@ object RNG {
 
   def ints(count: Int): Rand[List[Int]] = sequence(List.fill(count)(int))
 
-  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = {
-    rnd => {
-      val (va, rnda) = ra(rnd)
-      val (vb, rndb) = rb(rnda)
-      (f(va, vb), rndb)
-    }
-  }
+//  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = {
+//    rnd => {
+//      val (va, rnda) = ra(rnd)
+//      val (vb, rndb) = rb(rnda)
+//      (f(va, vb), rndb)
+//    }
+//  }
+
+  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    flatMap(ra){ a => flatMap(rb){ b => unit(f(a, b))}}
 
   def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
     fs.foldRight(unit(List.empty[A]))(map2(_, _)(_ :: _))
